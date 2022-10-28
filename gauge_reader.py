5c0821cf87811950d3180fc1bc8dc2e55a1f5943
jklhj <jklhj222@gmail.com>
2022-10-28 15:35:07 +0800

    first modify

--- gauge_reader.py
+++ gauge_reader.py
@@ -209,25 +209,41 @@ def findInitFianlAngle2(center: np.ndarray, init: np.ndarray, final: np.ndarray,
     rotate_v_ci = rm @ v_ci
     rotate_v_cf = rm @ v_cf
 
-def findMinMaxAngle(center: np.ndarray, init: np.ndarray, final: np.ndarray, delta_theta: int):
+def findMinMaxAngle(center: np.ndarray, init: np.ndarray, final: np.ndarray, tail: np.ndarray, delta_theta: int):
     v_ci = init - center
     v_cf = final - center
+    v_ctail = tail - center
     v_x = np.array([1, 0, 0])
+    v_y = np.array([0, 1, 0])
 
     print('v_ci, v_cf, v_x: ', v_ci, v_cf, v_x)
 
     norm_ci = np.linalg.norm(v_ci)
     norm_cf = np.linalg.norm(v_cf)
+    norm_ctail = np.linalg.norm(v_ctail)
     norm_x = np.linalg.norm(v_x)
+    norm_y = np.linalg.norm(v_y)
 
     angle_ci = np.arccos( v_ci.dot(v_x) / (norm_ci * norm_x) )
     angle_cf = np.arccos( v_cf.dot(v_x) / (norm_cf * norm_x) )
-    print('angle_ci, angle_cf: ', np.degrees(angle_ci+np.pi), np.degrees(angle_cf))
+    angle_ctail = np.arccos( v_ctail.dot(v_y) / (norm_ctail * norm_y) )
+
+    print('angle_ci, angle_cf, angle_ctail: ', np.degrees(angle_ci+np.pi), np.degrees(angle_cf), np.degrees(angle_ctail))
 
     rotated_angle_ci = angle_ci + np.pi + delta_theta
     rotated_angle_cf = angle_cf + delta_theta
+    direction_ctail = np.cross(v_ctail, v_y)[2] / np.abs(np.cross(v_ctail, v_y)[2]) * -1
+    if direction_ctail >= 0:
+        rotated_angle_ctail = 2*np.pi - angle_ctail + delta_theta
+        rotated_angle_chead = rotated_angle_ctail - np.pi
+    else:
+        rotated_angle_ctail = angle_ctail + delta_theta
+        rotated_angle_chead = rotated_angle_ctail + np.pi
 
-    return rotated_angle_ci , rotated_angle_cf
+    print('rotated_angle_ci, rotated_angle_cf, rotated_angle_chead: ', 
+          rotated_angle_ci, rotated_angle_cf, rotated_angle_chead)
+    
+    return rotated_angle_ci, rotated_angle_cf, rotated_angle_chead
 
 def findGaugeValue(results, img, min_value, max_value, min_angle_shift):
     objs = []
@@ -247,11 +263,13 @@ def findGaugeValue(results, img, min_value, max_value, min_angle_shift):
     center = np.append(np.array(results_dict[b'center'][:2]), 0)
     min_angle = np.append(np.array(results_dict[b'min_angle'][:2]), 0)
     max_angle = np.append(np.array(results_dict[b'max_angle'][:2]), 0)
+    tail_angle = np.append(np.array(results_dict[b'tail'][:2]), 0)
     print(center)
     delta_theta, rotation, rotate_img = rotate(img, center, min_angle, max_angle)
-    min_angle, max_angle = findMinMaxAngle(center, min_angle, max_angle, delta_theta)
+    min_angle, max_angle, pointer_angle = findMinMaxAngle(center, min_angle, max_angle, tail_angle, delta_theta)
     min_angle = np.degrees(min_angle)
     max_angle = np.degrees(max_angle)
+    pointer_angle = np.degrees(pointer_angle)
 
     print('min_angle, max_angle: ', min_angle, max_angle)
     print(np.degrees(delta_theta), rotation)
@@ -262,16 +280,17 @@ def findGaugeValue(results, img, min_value, max_value, min_angle_shift):
     # cvshow(rotate_img)
     # cv2.imwrite('test5.jpg', rotate_img)
 
-    last_deg = None
-    a = gauge_reader(rotate_img)
-    _, deg = a.get_deg(last_deg, show=False)
-    if 0 <= deg < 270:
-        last_deg = deg + 90
-    else:
-        last_deg = deg -270
+    # last_deg = None
+    # a = gauge_reader(rotate_img)
+    # _, deg = a.get_deg(last_deg, show=False)
+    # if 0 <= deg < 270:
+    #     last_deg = deg + 90
+    # else:
+    #     last_deg = deg -270
 
+    last_deg = pointer_angle
     gauge_value = (last_deg - min_angle - min_angle_shift) * value_per_degree
-    print('deg: ', deg, 'last deg: ', last_deg, 'max_angle: ', max_angle, 
+    print('last deg: ', last_deg, 'max_angle: ', max_angle, 
         'min_angle_shift: ', min_angle_shift, 'gauge_value: ', gauge_value)
 
     return gauge_value
@@ -285,11 +304,14 @@ if __name__ == '__main__':
 
     plt.figure(figsize=(2, 3))
 
-    for idx, img_f in enumerate(['030.jpg', '070.jpg', '150.jpg', 'frame_00000_rot5.jpg', 'frame_00000_rot-5.jpg']):
-    # for idx, img_f in enumerate(['frame_00000_rot5.jpg']):
+    # for idx, img_f in enumerate(['030.jpg', '070.jpg', '150.jpg', 'frame_00000_rot5.jpg', 'frame_00000_rot-5.jpg']):
+    for idx, img_f in enumerate(['150.jpg']):
+        init_t1 = time.time()
         img = cv2.imread(img_f)
         
         net, meta = readYoloConfig('config.txt')
+        
+        init_t2 = time.time()
         results = DFUNC.detect(net, meta, img, thresh=0.7)
 
         gauge_value = findGaugeValue(results, img, min_value, max_value, min_angle_shift)
@@ -299,4 +321,5 @@ if __name__ == '__main__':
         plt.title('Gauge_value: '+str(gauge_value), fontsize=12)
         plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
 
+        print('elpsed time: ', time.time() - init_t1, time.time() - init_t1)
     plt.show()
